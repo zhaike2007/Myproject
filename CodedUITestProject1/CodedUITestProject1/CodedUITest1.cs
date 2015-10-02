04fe95dcb6d8379b490335cfa3f65e1741ad6372
zhaike <jackzhai@hotmail.com>
2015-10-02 16:44:56 +0800

    11

--- CodedUITestProject1/CodedUITestProject1/CodedUITest1.cs
+++ CodedUITestProject1/CodedUITestProject1/CodedUITest1.cs
@@ -26,7 +26,7 @@ namespace CodedUITestProject1
         public void CodedUITestMethod1()
         {
 
-            Assert.AreEqual(1,1);
+            Assert.AreEqual(1,1);///
             //hellp world
             // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
         }
